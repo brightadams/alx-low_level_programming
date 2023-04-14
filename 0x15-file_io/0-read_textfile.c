88412ba6fd9ff9c3b576f3fe81bfcde80131a02d
Bright <brightadams58@gmail.com>
2023-04-14 15:25:53 -0700

    0 of 0x15

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,46 +1,39 @@
 #include "main.h"
+#include <stdlib.h>
 
 /**
- * read_textfile - this reads a text file and prints it to the standard output
- * @filename: this stores the name of the file to be read
- * @num_letters: this takes the number of letters to read and print
- * Return: the number of letters printed, or 0 if it failed
+ * read_textfile - Reads a text file and prints it to POSIX stdout.
+ * @filename: A pointer to the name of the file.
+ * @letters: The number of letters the
+ *           function should read and print.
+ *
+ * Return: If the function fails or filename is NULL - 0.
+ *         O/w - the actual number of bytes the function can read and print.
  */
-ssize_t read_textfile(const char *filename, size_t num_letters)
+ssize_t read_textfile(const char *filename, size_t letters)
 {
-    int fd;
-    int read_len, write_len;
-    char *buf;
-    
-    if (!filename)
-        return (0);
-    
-    fd = open(filename, O_RDONLY);
-    if (fd < 0)
-        return (0);
-    
-    buf = malloc(sizeof(char) * num_letters);
-    if (!buf)
-        return (0);
-    
-    read_len = read(fd, buf, num_letters);
-    if (read_len < 0)
-    {
-        free(buf);
-        return (0);
-    }
-    
-    buf[read_len] = '\0';
-    close(fd);
-    
-    write_len = write(STDOUT_FILENO, buf, read_len);
-    if (write_len < 0)
-    {
-        free(buf);
-        return (0);
-    }
-    
-    free(buf);
-    return (write_len);
-}
+	ssize_t o, r, w;
+	char *buffer;
 
+	if (filename == NULL)
+		return (0);
+
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+		return (0);
+
+	o = open(filename, O_RDONLY);
+	r = read(o, buffer, letters);
+	w = write(STDOUT_FILENO, buffer, r);
+
+	if (o == -1 || r == -1 || w == -1 || w != r)
+	{
+		free(buffer);
+		return (0);
+	}
+
+	free(buffer);
+	close(o);
+
+	return (w);
+}
